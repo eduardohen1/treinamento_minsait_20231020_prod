4a1a46ac932320326a7cbd3f03f48e3667214692
eduardohen1 <eduardohmferreira@gmail.com>
2023-10-21 15:48:38 -0300

    Salvando projeto 07

--- src/main/java/br/com/ehmf/AppProdutos/service/ProdutoService.java
+++ src/main/java/br/com/ehmf/AppProdutos/service/ProdutoService.java
@@ -1,5 +1,55 @@
 package br.com.ehmf.AppProdutos.service;
 
-public class ProdutoService {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ehmf.AppProdutos.model.Produto;
+import br.com.ehmf.AppProdutos.repository.ProdutoRepository;
+import br.com.ehmf.AppProdutos.service.interfaces.ProdutoServiceInterface;
+
+@Service
+public class ProdutoService implements ProdutoServiceInterface {
+
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	public ProdutoService(ProdutoRepository produtoRepository) {
+		this.produtoRepository = produtoRepository;
+	}
+	
+	@Override
+	public Produto save(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	@Override
+	public Optional<Produto> getById(Long id) {
+		return produtoRepository.findById(id);
+	}
+
+	@Override
+	public List<Produto> getAll() {
+		return produtoRepository.findAll();
+	}
+
+	@Override
+	public Produto update(Produto produto) {
+		//encontrei o produto
+		Optional<Produto> upProduto = produtoRepository.findById(produto.getId());
+		
+		
+		
+		
+		return null;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }

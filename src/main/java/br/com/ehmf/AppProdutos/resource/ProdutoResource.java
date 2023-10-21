67dd30c34547d02611e10e721c098ceb15fc90a5
eduardohen1 <eduardohmferreira@gmail.com>
2023-10-21 16:30:25 -0300

    Salvando projeto 11

--- src/main/java/br/com/ehmf/AppProdutos/resource/ProdutoResource.java
+++ src/main/java/br/com/ehmf/AppProdutos/resource/ProdutoResource.java
@@ -1,5 +1,33 @@
 package br.com.ehmf.AppProdutos.resource;
 
-public class ProdutoResource {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ehmf.AppProdutos.model.Produto;
+import br.com.ehmf.AppProdutos.service.ProdutoService;
+
+@RestController
+@RequestMapping("/api/produtos") //http://localhosto:8080/api/produtos
+public class ProdutoResource {
+	
+	private ProdutoService produtoService;
+	
+	@Autowired
+	public ProdutoResource(ProdutoService produtoService) {
+		this.produtoService = produtoService;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAllProdutos(){
+		List<Produto> produtos = produtoService.getAll();
+		if(produtos == null)
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(produtos);
+	}
+	
 }

7d8299e17aae2b6599fb30d48f65fc7739d71a14
eduardohen1 <eduardohmferreira@gmail.com>
2023-10-21 16:42:48 -0300

    Salvando projeto 12

--- src/main/java/br/com/ehmf/AppProdutos/resource/ProdutoResource.java
+++ src/main/java/br/com/ehmf/AppProdutos/resource/ProdutoResource.java
@@ -1,10 +1,18 @@
 package br.com.ehmf.AppProdutos.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +20,7 @@ import br.com.ehmf.AppProdutos.model.Produto;
 import br.com.ehmf.AppProdutos.service.ProdutoService;
 
 @RestController
-@RequestMapping("/api/produtos") //http://localhosto:8080/api/produtos
+@RequestMapping("/api/produtos") //http://localhost:8080/api/produtos
 public class ProdutoResource {
 	
 	private ProdutoService produtoService;
@@ -28,6 +36,36 @@ public class ProdutoResource {
 		if(produtos == null)
 			return ResponseEntity.notFound().build();
 		return ResponseEntity.ok(produtos);
+	}
+	
+	@GetMapping("/{id}") //http://localhost:8080/api/produtos/12
+	public ResponseEntity<Optional<Produto>> getById(@PathVariable Long id){
+		Optional<Produto> produto = produtoService.getById(id);
+		if(produto == null)
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(produto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Produto> save(@RequestBody Produto produto){
+		Produto newProduto = produtoService.save(produto);
+		if(newProduto == null)
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(newProduto);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> update(@RequestBody Produto produto){
+		Produto newProduto = produtoService.update(produto);
+		if(newProduto == null)
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(newProduto);
+	}
+	
+	@DeleteMapping("{/id}")
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		produtoService.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
 }

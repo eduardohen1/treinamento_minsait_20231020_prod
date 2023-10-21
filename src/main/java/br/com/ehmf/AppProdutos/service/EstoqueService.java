06ea418c2b02f7eddea283b954b4eb54ec7000be
eduardohen1 <eduardohmferreira@gmail.com>
2023-10-21 16:12:26 -0300

    Salvando projeto 09

--- src/main/java/br/com/ehmf/AppProdutos/service/EstoqueService.java
+++ src/main/java/br/com/ehmf/AppProdutos/service/EstoqueService.java
@@ -1,5 +1,71 @@
 package br.com.ehmf.AppProdutos.service;
 
-public class EstoqueService {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ehmf.AppProdutos.model.Estoque;
+import br.com.ehmf.AppProdutos.repository.EstoqueRepository;
+import br.com.ehmf.AppProdutos.service.interfaces.EstoqueServiceInterface;
+
+@Service
+public class EstoqueService implements EstoqueServiceInterface {
+	
+	private EstoqueRepository estoqueRepository;
+	
+	@Autowired
+	public EstoqueService(EstoqueRepository estoqueRepository) {
+		this.estoqueRepository = estoqueRepository;
+	}
+	
+	@Override
+	public Estoque save(Estoque estoque) {
+		return estoqueRepository.save(estoque);
+	}
+
+	@Override
+	public Optional<Estoque> getById(Long id) {
+		return estoqueRepository.findById(id);
+	}
+
+	@Override
+	public List<Estoque> getAll() {
+		return estoqueRepository.findAll();
+	}
+
+	@Override
+	public Estoque update(Estoque estoque) {
+		Optional<Estoque> upEstoque = estoqueRepository.findById(estoque.getId());
+		
+		if(upEstoque.isPresent()) {
+			Estoque newEstoque = upEstoque.get();
+			newEstoque.setProduto(estoque.getProduto());
+			newEstoque.setQuantidade(estoque.getQuantidade());
+			return estoqueRepository.save(newEstoque);
+		}
+		return estoque;
+	}
+
+	@Override
+	public void delete(Long id) {
+		estoqueRepository.deleteById(id);
+	}
+
+	@Override
+	public Estoque addQuantidade(Long id, int quantidade) {
+		return estoqueRepository.findById(id)
+				.map(estoque -> {
+					estoque.setQuantidade(estoque.getQuantidade() + quantidade);
+					return estoqueRepository.save(estoque);
+				});
+	}
+
+	@Override
+	public Estoque delQuantidade(Long id, int quantidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
